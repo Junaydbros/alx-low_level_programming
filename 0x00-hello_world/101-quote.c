750da48c5904494b33442c2d50c719f4994f7bfd
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-16 14:46:59 +0000

    Attempt 01

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a text to the standard error\
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", str);
+
+	return (0);
+}
