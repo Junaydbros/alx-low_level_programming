11dbfb5349f056f96ca4aefa4dba798fab81a855
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-16 14:21:18 +0000

    Attempt 01

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a text
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
