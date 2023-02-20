43197c2b4cf5332072238c3f92810b8d46817838
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-20 07:11:56 +0000

    Attempt 01

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible different combinationof two digits
+ * The program ensures that the numbers are separated by ',' and a space
+ * The program shows that the two digits are different
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 8; m++)
+
+	{
+		for (n = 0; n <= 7; n++)
+		{
+			putchar ((m % 10) + '0');
+			putchar ((n % 10) + '0');
+
+					if (m == 8 && n == 7)
+					{
+						continue;
+
+
+	putchar (',');
+	putchar (' ');
+					}
+		}
+	}
+	putchar ('\n');
+}
