9ff39e282d8595ada9ec726cba0d0c02cced8a9b
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-21 15:35:57 +0000

    Attempt 08

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,14 +13,13 @@ void print_alphabet_x10(void)
 	char c;
 	int a;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-
 	a = 0;
 	while (a <= 10)
 	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 		a++;
 	}
 
