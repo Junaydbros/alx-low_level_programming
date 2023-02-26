71f82893def72d98622cd1b0571e0ce3ff9d4141
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-26 16:24:21 +0000

    Attempt 17

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,6 +15,7 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
+			c = a * b;
 			_putchar(' ');
 
 			if (c <= 9)
@@ -24,7 +25,6 @@ void times_table(void)
 			}
 			else
 			{
-				c = a * b;
 
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
