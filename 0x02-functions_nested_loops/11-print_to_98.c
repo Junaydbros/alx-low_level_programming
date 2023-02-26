3372fafa4baf5e0a9ac84dc1e382c2d072d83e62
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-26 20:42:01 +0000

    Attempt 16

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,28 +14,23 @@ void print_to_98(int n)
 {
 	int a;
 
-	for (a = n; a <= 98; a++)
+	if (n < 98)
 	{
-		if (a != 98)
+		for (a = n; a <= 98; a++)
 		{
 			printf("%d, ", a);
 		}
 
-		else
+	printf("%d\n", a);
+	}
+
+	else
+	{
+		for (a = n; a >= 98; a--)
 		{
-			printf("%d\n", a);
+		printf("%d, ", a);
 		}
 	}
 
-	for (a = n; a >= 98; a--)
-	{
-		if (a != 98)
-		{
-			printf("%d, ", a);
-		}
-		else
-		{
-			printf("%d\n", a);
-		}
-	}
+	printf("%d\n", a);
 }
