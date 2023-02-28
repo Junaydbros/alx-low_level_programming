5c5c4d6dcc2d5ef207d8a4083a230352c7d8cadf
Abdulhamid <hamidejisanusi@gmail.com>
2023-02-28 20:13:58 +0000

    Attempt 01

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program testing fizzbuzz in a set of numbers
+ * In this case, we'd be working with numbers from 1 - 100
+ * For every multiple of three, the program prints 'Fizz' instead of the number
+ * For every multiple of five, the program prints 'Buzz' instead of the number
+ * For numbers which are multiples of both 3 & 5, the program prints 'FizzBuzz'
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+
+	_putchar('\n');
+}
