1d8f76955a9398cebaa978cec57935cf83495bc3
Abdulhamid <hamidejisanusi@gmail.com>
2023-03-03 19:26:52 +0000

    Attempt 07

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -22,9 +22,9 @@ void print_triangle(int size)
 	else
 	{
 
-		for (x = 1; x <= size; x++)
+		for (x = 0; x <= size; x++)
 		{
-			for (y = 1; y <= size; y++)
+			for (y = 0; y <= size; y++)
 			{
 				if ((x + y) <= size)
 				{
