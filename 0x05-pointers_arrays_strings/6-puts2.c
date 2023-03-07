10ca6c9c5341b87653cc540d4278ce9cc59717f7
Junaydbros <hamidejisanusi@gmail.com>
2023-03-07 19:16:42 +0000

    Attempt 01

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string
+ * The printout starts with the first character, followed by a new line.
+ * @str: the character to be alternated in the whole string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a = 0, b;
+
+	while (str[a] != '\0')
+	{
+		str[a] = str[a] + 1;
+
+		_putchar(*str);
+		a++;
+	}
+
+	_putchar('\n');
+}
