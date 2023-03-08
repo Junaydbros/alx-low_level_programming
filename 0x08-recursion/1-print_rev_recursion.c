d7fd0ec6fa55f29ce452b50619f9d43f33938416
Junaydbros <hamidejisanusi@gmail.com>
2023-03-08 21:09:46 +0000

    Attempt 02

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,14 +10,12 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s < '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-
 		return;
 	}
 
-	_print_rev_recursion(s - 1);
+	_print_rev_recursion(s + 1);
 
-	_putchar('\n');
+	_putchar(*s);
 }
