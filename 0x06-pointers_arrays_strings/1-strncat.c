89686b7d8bc0aa85b5eb3448e3f533e611b52f4a
Junaydbros <hamidejisanusi@gmail.com>
2023-03-08 00:44:20 +0000

    Attempt 09

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,10 +22,11 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[a] != '\0')
 	{
 		a++;
+		continue;
 	}
 
 	b = 0;
-	while (src[b] != '\0' && b < n)
+	while (src[b] != '\0')
 	{
 		b++;
 	}
