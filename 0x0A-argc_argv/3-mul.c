d7471dfb4088f5e49a0fff5a7ed2b2dd817749b9
Junaydbros <hamidejisanusi@gmail.com>
2023-03-12 22:59:02 +0000

    Attempt 07

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,17 +17,18 @@ int main(int argc, char *argv[])
 {
 	int prod;
 
-	if (argc <= 2)
-	{
-		printf("Error\n");
-
-		return (1);
-	}
-	else
+	if (argc >= 2)
 	{
 		prod = atoi(argv[1]) * atoi(argv[2]);
 
 		printf("%d\n", prod);
+	}
+
+	else
+	{
+		printf("Error\n");
+
+		return (1);
 	}
 
 	return (0);
