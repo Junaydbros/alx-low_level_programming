5536dfbc3e7638ad339f49e615ec60a92f16bcc0
Junaydbros <hamidejisanusi@gmail.com>
2023-03-13 17:23:54 +0000

    Adjusting number of lines

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - tests function that prints if integer is positive
  * or if integer is negative.
  *
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
