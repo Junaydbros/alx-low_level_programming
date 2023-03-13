417c83405e621437989a0c2f1d8c2de7ff878464
Junaydbros <hamidejisanusi@gmail.com>
2023-03-13 19:51:30 +0000

    Attempt 01

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
 	if (size == 0)
 	{
@@ -29,7 +30,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	*ptr = c;
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
 
 	return (ptr);
 }
