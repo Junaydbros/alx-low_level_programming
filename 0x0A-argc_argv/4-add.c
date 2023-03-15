5dd7cc2f1d06ba16f3f9e7eec45820acf89e7f99
Junaydbros <hamidejisanusi@gmail.com>
2023-03-15 23:15:54 +0000

    Temporary working

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 	char *rmv;
 
 	if (argc < 2)
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		strtol(argv[i], &rmv, 10);
+		j = strtol(argv[i], &rmv, 10);
 
 		if (*rmv)
 		{
@@ -34,8 +34,8 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-		sum += atoi(argv[i]);
-		/* sum += j; */
+		/* sum += atoi(argv[i]); */
+		sum += j;
 		}
 	}
 
