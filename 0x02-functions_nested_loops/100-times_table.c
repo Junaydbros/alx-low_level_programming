01401a369b909dada8e474b435da1814a7729f04
Junaydbros <hamidejisanusi@gmail.com>
2023-03-19 14:55:49 +0000

    Updating all changes with nested loops directory

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n < 15 || n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (a = 0; a <= n; a++)
 		{
@@ -25,12 +25,22 @@ void print_times_table(int n)
 
 				c = a * b;
 
-				if (c <= n)
+				if (c <= 9)
 				{
 					_putchar(' ');
 				}
 
-				else
+				if (c <= 99)
+				{
+					_putchar(' ');
+				}
+
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar((c / 10) % 10 + '0');
+				}
+				else if (c <= 99 && c >= 10)
 				{
 					_putchar((c / 10) + '0');
 				}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that computes and prints the sum of all multiples of 3 or 5
+ * These multiples below 1024 should be printed
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int n;
+
+	if (n < 1024)
+	{
+		if (n % 3 == 0)
+		{
