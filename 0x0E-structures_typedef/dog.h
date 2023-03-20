4d1ba64a58a046d9e321d1b30ecda481ddaeb2d2
Junaydbros <hamidejisanusi@gmail.com>
2023-03-20 22:25:53 +0000

    New errors fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,12 @@
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 
-/* void init_dog(struct dog *d, char *name, float age, char *owner); */
-/* void print_dog(struct dog *d); */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 /* Junaydbros */
 #endif
