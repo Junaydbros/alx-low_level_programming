78c551ae43b8b77c280fdae76a7d117225268c9a
Junaydbros <hamidejisanusi@gmail.com>
2023-04-05 21:43:14 +0000

    Error Fixes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -43,7 +43,8 @@ void print_all(const char * const format, ...)
 				str = va_arg(anyprint, char *);
 
 				if (str == NULL)
-					printf("(nil)");
+					str = ("(nil)");
+				printf("%s", str);
 				break;
 			default:
 				s++;
