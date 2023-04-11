395c70d4cac8185e9b85043a44df4a57821f6526
Junaydbros <hamidejisanusi@gmail.com>
2023-04-11 00:30:32 +0000

    Function to convert a binary number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint -  a function that converts binary number to an unsigned int
+ * 'b' would be pointing to a string of 0 and 1 chars
+ * @b: a pointer to a string
+ *
+ * Return: an unsigned int converted from binary
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	unsigned int uint = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	len = 0;
+	while (b[len] != '\0')
+	{
+		if (b[len] != '0' || b[len] == '1')
+		{
+			return (0);
+		}
+		else
+		{
+			uint = (uint << 1) +  (b[len] - '0');
+			len++;
+		}
+	}
+	return (uint);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+This directory contains all projects introducing the concept and testing the basic understanding of bit manipulation, it's meaning, usage, application and implementation in C programming.
+
+This program also helped understand what bitwise operators are, how to manipulate bits and use bitwise operators, it's relationship with logical operators and how these operate function in the understanding of bit manipulation.
+
+Resources utilized for this concept include
+
+https://youtu.be/egUyaWtsQc0
+
+https://youtu.be/LP0acaj3ZLE
